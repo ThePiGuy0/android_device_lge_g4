999a2cc27151d320e0dfc88fac33c46f5f0a6026
ThePiGuy <thepiguy0@gmail.com>
2019-07-20 15:49:08 +0100

    Rebrand to Omnirom

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,38 +1,38 @@
 export ALLOW_MISSING_DEPENDENCIES=true
 export USE_NINJA=true
 
-add_lunch_combo lineage_f500_usu-userdebug
-add_lunch_combo lineage_f500_usu-user
-add_lunch_combo lineage_f500_usu_eng
+add_lunch_combo omni_f500_usu-userdebug
+add_lunch_combo omni_f500_usu-user
+add_lunch_combo omni_f500_usu_eng
 
-add_lunch_combo lineage_h810_usu-userdebug
-add_lunch_combo lineage_h810_usu-user
-add_lunch_combo lineage_h810_usu_eng
+add_lunch_combo omni_h810_usu-userdebug
+add_lunch_combo omni_h810_usu-user
+add_lunch_combo omni_h810_usu_eng
 
-add_lunch_combo lineage_h811_usu-userdebug
-add_lunch_combo lineage_h811_usu-user
-add_lunch_combo lineage_h811_usu_eng
+add_lunch_combo omni_h811_usu-userdebug
+add_lunch_combo omni_h811_usu-user
+add_lunch_combo omni_h811_usu_eng
 
-add_lunch_combo lineage_h812_usu-userdebug
-add_lunch_combo lineage_h812_usu-user
-add_lunch_combo lineage_h812_usu_eng
+add_lunch_combo omni_h812_usu-userdebug
+add_lunch_combo omni_h812_usu-user
+add_lunch_combo omni_h812_usu_eng
 
-add_lunch_combo lineage_h815-userdebug
-add_lunch_combo lineage_h815-user
-add_lunch_combo lineage_h815_eng
+add_lunch_combo omni_h815-userdebug
+add_lunch_combo omni_h815-user
+add_lunch_combo omni_h815_eng
 
-add_lunch_combo lineage_h815_usu-userdebug
-add_lunch_combo lineage_h815_usu-user
-add_lunch_combo lineage_h815_usu_eng
+add_lunch_combo omni_h815_usu-userdebug
+add_lunch_combo omni_h815_usu-user
+add_lunch_combo omni_h815_usu_eng
 
-add_lunch_combo lineage_ls991_usu-userdebug
-add_lunch_combo lineage_ls991_usu-user
-add_lunch_combo lineage_ls991_usu_eng
+add_lunch_combo omni_ls991_usu-userdebug
+add_lunch_combo omni_ls991_usu-user
+add_lunch_combo omni_ls991_usu_eng
 
-add_lunch_combo lineage_us991_usu-userdebug
-add_lunch_combo lineage_us991_usu-user
-add_lunch_combo lineage_us991_usu_eng
+add_lunch_combo omni_us991_usu-userdebug
+add_lunch_combo omni_us991_usu-user
+add_lunch_combo omni_us991_usu_eng
 
-add_lunch_combo lineage_vs986_usu-userdebug
-add_lunch_combo lineage_vs986_usu-user
-add_lunch_combo lineage_vs986_usu_eng
+add_lunch_combo omni_vs986_usu-userdebug
+add_lunch_combo omni_vs986_usu-user
+add_lunch_combo omni_vs986_usu_eng
